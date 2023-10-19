7c6f83522ebc0abb531a3b2b429643b722deb3b8
monishat <monishat@gmail.com>
2023-10-19 11:54:04 +0600

    Add put request for product Update

--- index.js
+++ index.js
@@ -25,39 +25,59 @@ const run = async () => {
         const productCollection = client.db('TechStore').collection('products');
         const brandCollection = client.db('TechStore').collection('brands');
 
-        app.get('/products', async(req, res) => {
+        app.get('/products', async (req, res) => {
             const products = await productCollection.find().toArray();
             res.send(products);
         });
 
-        app.get('/brands', async(req, res) => {
+        app.get('/brands', async (req, res) => {
             const brands = await brandCollection.find().toArray();
             res.send(brands);
         });
 
-        app.get('/products/:id', async(req, res) => {
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const product = await productCollection.findOne({_id: new ObjectId(id)});
+            const product = await productCollection.findOne({ _id: new ObjectId(id) });
             res.send(product);
         });
 
-        app.get('/products/brand/:brand', async(req, res) => {
+        app.get('/products/brand/:brand', async (req, res) => {
             const brand = req.params.brand;
-            const product = await productCollection.find({brand_name: brand}).toArray();
+            const product = await productCollection.find({ brand_name: brand }).toArray();
             res.send(product);
         });
 
 
-        app.post('/products', async(req, res) => {
+        app.post('/products', async (req, res) => {
             const product = req.body;
             const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const product = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const option = { upsert: true }
+            const updatedProduct = {
+                $set: {
+                    name: product.name,
+                    brand_name: product.brandName,
+                    type: product.type,
+                    price: product.price,
+                    rating: product.rating,
+                    image: product.image,
+                    description: product.shortDescription
+                }
+            }
+            const result = await productCollection.updateOne(filter, updatedProduct, option);
             res.send(result);
         })
 
         await client.db('admin').command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
     finally {
